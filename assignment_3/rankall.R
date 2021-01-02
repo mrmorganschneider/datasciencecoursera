77976015c03dbb644737e97829f142ac44a19103
Morgan Schneider <mr.cardholder.osi@gmail.com>
2021-01-02 17:00:17 -0600

    work on rankall continues

---
+++ assignment_3/rankall.R
@@ -0,0 +1,53 @@
+rankall <- function(outcome, ranking = "best") {
+     library(dplyr)
+
+    #changes cases for inputs
+    outcome <- tolower(outcome)
+
+    #checks to make sure condition input is valid
+    if (!(outcome == "heart attack"
+        || outcome == "heart failure"
+        || outcome == "pneumonia")){
+            stop("Invalid Outcome")
+        }
+
+    hosData <- read.csv("outcome-of-care-measures.csv", 
+    colClasses = "character")
+
+    #pulls appropriate data based on the condition specified
+    switch( outcome,
+        "heart attack" = hosData <- hosData[,c(2,7,11)],
+        "heart failure" = hosData <- hosData[,c(2,7,17)],
+        "pneumonia" = hosData <- hosData[,c(2,7,23)]
+    )
+
+    #filters out Not Availables and sorts data
+    names(hosData) <- c("Hospital", "State", "Rate")
+    hosData <- filter(hosData, hosData[,3] != "Not Available")
+    hosData$Rate <- as.numeric(hosData$Rate) 
+    hosData <- hosData[order(hosData$State, hosData$Rate),]
+
+    hosData$Rank <- 1
+    stateName <- "XX"
+    rank <- 1
+
+    for(i in 1:nrow(hosData)){
+        if(hosData[i,2] != stateName){
+            rank <- 1
+            stateName <- hosData[i,2]
+        }
+
+        hosData[i,4] <- rank
+
+        rank <- rank + 1
+
+    }
+
+    if(ranking == "best") {return(subset(hosData,Rank == 1))}
+
+    else if(ranking == "wort"){
+
+    }
+    
+    else {return(subset(hosData, Rank == ranking))}
+}
